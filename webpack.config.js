72756a4ddbcd56ae8c2963d6ef9710e949c671c9
YeongG <yeonggil223@gmail.com>
2020-10-21 19:30:24 +0900

    [MODIFY] webpack file-loader 추가

--- webpack.config.js
+++ webpack.config.js
@@ -4,32 +4,40 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
   mode: "none",
   entry: {
-    app: path.join(__dirname, "src", "index.tsx"),
+    app: path.join(__dirname, "src", "index.tsx")
   },
   target: "web",
   resolve: {
-    extensions: [".ts", ".tsx", ".js"],
+    extensions: [".ts", ".tsx", ".js"]
   },
   module: {
     rules: [
       {
         test: /\.tsx?$/,
         use: "ts-loader",
-        exclude: "/node_modules/",
+        exclude: "/node_modules/"
       },
-    ],
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        use: [
+          {
+            loader: "file-loader"
+          }
+        ]
+      }
+    ]
   },
   output: {
     filename: "bundle.min.js",
-    path: path.resolve(__dirname, "AWS"),
+    path: path.resolve(__dirname, "AWS")
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.join(__dirname, "public", "index.html"),
-    }),
+      template: path.join(__dirname, "public", "index.html")
+    })
   ],
   devServer: {
-    overlay: true,
     publicPath: "/",
-  },
+    historyApiFallback: true
+  }
 };
