042d3e6a8f1b3fcf239dc1123e0f4969fdb9a874
YeongG <yeonggil223@gmail.com>
2020-11-01 23:33:36 +0900

    [MODIFY] UI/UX -> 기능 추가

--- src/component/Modal/FormModal.tsx
+++ src/component/Modal/FormModal.tsx
@@ -1,20 +1,39 @@
 import React, { FC } from "react";
+import { useEffect } from "react";
+import { useRef } from "react";
+import { useCallback } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { modalActionCreater } from "../../module/action/modal";
+import { StoreType } from "../../module/reducer";
 import * as S from "./styles";
 
-interface ModalProps {
-  title: string;
-  subTitle: string;
-}
+const FormModal: FC = () => {
+  const ref = useRef<HTMLButtonElement | null>(null);
+  const { isOpen, subTitle, title } = useSelector(
+    (store: StoreType) => store.modal.formModal
+  );
 
-const FormModal: FC<ModalProps> = ({ title, subTitle }) => {
+  useEffect(() => {
+    ref.current && ref.current.focus();
+  }, [isOpen]);
+
+  const dispatch = useDispatch();
+
+  const closeModal = useCallback(() => {
+    dispatch(modalActionCreater.formModalOff());
+  }, []);
   return (
-    <S.Container>
-      <S.Modal>
-        <S.Title>{title}</S.Title>
-        <S.SubTitle>{subTitle}</S.SubTitle>
-        <S.OkBtn>확인</S.OkBtn>
-      </S.Modal>
-    </S.Container>
+    isOpen && (
+      <S.Container>
+        <S.Modal>
+          <S.Title>{title}</S.Title>
+          <S.SubTitle>{subTitle}</S.SubTitle>
+          <S.OkBtn ref={ref} onClick={closeModal}>
+            확인
+          </S.OkBtn>
+        </S.Modal>
+      </S.Container>
+    )
   );
 };
 
