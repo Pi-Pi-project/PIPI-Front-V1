9d78c8c34274a10d8827418e00c4cc54cfac019d
YeongG <yeonggil223@gmail.com>
2020-11-01 23:32:53 +0900

    [MODIFY] UI/UX -> 기능 추가

--- src/component/Login/LoginFormFooter/LoginFormFooter.tsx
+++ src/component/Login/LoginFormFooter/LoginFormFooter.tsx
@@ -1,12 +1,23 @@
 import React, { FC } from "react";
+import { useEffect } from "react";
+import { useRef } from "react";
+import { useCallback } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import { loginActionCreater } from "../../../module/action/login";
+import { StoreType } from "../../../module/reducer";
 import * as S from "../styles";
 
 const LoginFormFooter: FC = () => {
+  const dispatch = useDispatch();
+  const requestLogin = useCallback(() => {
+    dispatch(loginActionCreater.requestLoginSaga());
+  }, []);
   return (
     <S.LoginFooter>
-      <S.LoginButton>로그인</S.LoginButton>
+      <S.LoginButton onClick={requestLogin}>로그인</S.LoginButton>
       <S.ForgotPassword>
-        <a>계정이 아직 없으신가요?</a>
+        <Link to="/register">계정이 아직 없으신가요?</Link>
       </S.ForgotPassword>
     </S.LoginFooter>
   );
