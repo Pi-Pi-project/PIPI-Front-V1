737e7514405f0011ac4af87ab77b6b8cab3b751e
YeongG <yeonggil223@gmail.com>
2020-11-01 23:31:30 +0900

    [MODIFY] UI/UX -> 기능 추가

--- src/component/FindPassword/FindPasswordFormFooter/FindPasswordFormFooter.tsx
+++ src/component/FindPassword/FindPasswordFormFooter/FindPasswordFormFooter.tsx
@@ -1,10 +1,25 @@
 import React, { FC } from "react";
+import { useCallback } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { passwordActionCreater } from "../../../module/action/password";
+import { StoreType } from "../../../module/reducer";
 import * as S from "../styles";
 
 const FindPasswordFormFooter: FC = () => {
+  const dispatch = useDispatch();
+  const step = useSelector((store: StoreType) => store.password.step);
+  const findEmail = useCallback(() => {
+    dispatch(passwordActionCreater.findEmailSaga());
+  }, []);
   return (
     <S.FindPasswordFooter>
-      <S.FindPasswordSubmitBtn>변경</S.FindPasswordSubmitBtn>
+      {step === "CHECK_EMAIL" ? (
+        <S.FindPasswordSubmitBtn onClick={findEmail}>
+          보내기
+        </S.FindPasswordSubmitBtn>
+      ) : (
+        ""
+      )}
     </S.FindPasswordFooter>
   );
 };
