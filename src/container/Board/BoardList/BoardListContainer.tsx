adb452f3e79058dba64246241e34466f4bcb0b8b
YeongG <yeonggil223@gmail.com>
2020-11-01 23:35:57 +0900

    [CREATE] BoardList container

---
+++ src/container/Board/BoardList/BoardListContainer.tsx
@@ -0,0 +1,8 @@
+import React, { FC } from "react";
+import { BoardList } from "../../../component";
+
+const BoardListContainer: FC = () => {
+  return <BoardList />;
+};
+
+export default BoardListContainer;
