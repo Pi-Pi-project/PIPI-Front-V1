596fec680607d130000acf5b518398f240d401dc
YeongG <yeonggil223@gmail.com>
2020-11-01 23:29:14 +0900

    [MODIFY] asset export default 추가

--- src/asset/index.ts
+++ src/asset/index.ts
@@ -1,1 +1,4 @@
 export { default as formBannerJpg } from "./form/formBanner.jpg";
+export { default as pipiLogoSvg } from "./navigation/pipi-logo.svg";
+export { default as magnifyingIconSvg } from "./board/magnifying-icon.svg";
+export { default as pictureIcon } from "./form/picture.svg";
