5c69f37e9f9aab11fe85f8aade9cf1198995c2d1
YeongG <yeonggil223@gmail.com>
2020-11-02 23:52:25 +0900

    [MODIFY] container export default 추가

--- src/container/index.ts
+++ src/container/index.ts
@@ -2,3 +2,4 @@ export { default as LoginContainer } from "./Login/LoginContainer";
 export { default as FindPasswordContainer } from "./FindPassword/FindPasswordContainer";
 export { default as RegisterContainer } from "./Register/RegisterContainer";
 export { default as BoardListContainer } from "./Board/BoardList/BoardListContainer";
+export { default as BoardWriteContainer } from "./Board/BoardWrite/BoardWriteContainer";
