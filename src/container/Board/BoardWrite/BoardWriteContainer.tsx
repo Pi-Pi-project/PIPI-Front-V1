aa2df337af9aac205be7245e23e008b3b93b5e3d
YeongG <yeonggil223@gmail.com>
2020-11-02 23:52:06 +0900

    [CREATE] BoardWrite container

---
+++ src/container/Board/BoardWrite/BoardWriteContainer.tsx
@@ -0,0 +1,8 @@
+import React, { FC } from "react";
+import { BoardWrtie } from "../../../component";
+
+const BoardWriteContainer: FC = () => {
+  return <BoardWrtie />;
+};
+
+export default BoardWriteContainer;
