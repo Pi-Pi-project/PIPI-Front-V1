9046c79e31f8574de078d7b60f10efb52ff20a2b
YeongG <yeonggil223@gmail.com>
2020-11-02 23:42:51 +0900

    [MODIFY] redux saga rootSaga 추가

--- src/module/saga/index.ts
+++ src/module/saga/index.ts
@@ -1,10 +1,11 @@
 import { all } from "redux-saga/effects";
+import boardSaga from "./board";
 import loginSaga from "./login";
 import passwordSaga from "./password";
 import registerSaga from "./register";
 
 function* rootSaga() {
-  yield all([loginSaga(), passwordSaga(), registerSaga()]);
+  yield all([loginSaga(), passwordSaga(), registerSaga(), boardSaga()]);
 }
 
 export default rootSaga;
