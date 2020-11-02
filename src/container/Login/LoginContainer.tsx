1c8cca0aa8a1ab71de3dab182d0049bd73f0565e
YeongG <yeonggil223@gmail.com>
2020-11-02 23:52:42 +0900

    [MODIFY] login container

--- src/container/Login/LoginContainer.tsx
+++ src/container/Login/LoginContainer.tsx
@@ -1,7 +1,20 @@
 import React, { FC } from "react";
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { useHistory } from "react-router";
 import { Login } from "../../component";
+import { StoreType } from "../../module/reducer";
 
 const LoginContainer: FC = () => {
+  const history = useHistory();
+  const successLogin = useSelector(
+    (store: StoreType) => store.login.successLogin
+  );
+
+  useEffect(() => {
+    successLogin && history.push("/board");
+  }, [successLogin]);
+
   return <Login />;
 };
 
