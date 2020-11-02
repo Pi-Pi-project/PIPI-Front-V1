3523394cf1b42f4baf1741b82de4d27e5695e451
YeongG <yeonggil223@gmail.com>
2020-11-02 23:42:32 +0900

    [MODIFY] component export default 추가

--- src/component/index.ts
+++ src/component/index.ts
@@ -1,3 +1,4 @@
 export { default as Login } from "./Login/Login";
 export { default as FindPassword } from "./FindPassword/FindPassword";
 export { default as BoardList } from "./Board/BoardList/BoardList";
+export { default as BoardWrtie } from "./Board/BoardWrite/BoardWrite";
