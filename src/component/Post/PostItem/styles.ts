abeb93f8c1f20db8f5e4141fd801e9a9b8f5ed41
YeongG <yeonggil223@gmail.com>
2020-11-02 23:49:43 +0900

    [MODIFY] 폴더 경로 이동

--- src/component/Post/PostItem/styles.ts
+++ src/component/Post/PostItem/styles.ts
@@ -19,14 +19,38 @@ export const ProjectIntroduce = styled.div`
   flex: 1;
   background-color: #f8f9fc;
   box-sizing: border-box;
-  padding: 0 15px;
+  padding: 0 10px;
   display: flex;
   flex-direction: column;
 `;
 
+export const UserImg = styled.div<{ src: string }>`
+  background: url(${props => props.src}) no-repeat;
+  background-position: center;
+  background-size: cover;
+  width: 44px;
+  height: 44px;
+  border-radius: 14px;
+`;
+
+export const SkillImg = styled.div<{ src: string }>`
+  background: url(${props => props.src}) no-repeat;
+  background-position: center;
+  background-size: cover;
+  width: 50px;
+  height: 50px;
+`;
+
+export const UserName = styled.div`
+  font-size: 25px;
+  color: #3b3b3b;
+  margin-left: 15px;
+`;
+
 export const ProjectManager = styled.div`
   height: 60px;
-  background-color: red;
+  display: flex;
+  box-sizing: border-box;
 `;
 
 export const Introduce = styled.div`
@@ -50,5 +74,6 @@ export const ProjectName = styled.div`
 
 export const SkillSets = styled.div`
   flex: 1;
-  background-color: blue;
 `;
+
+export const SKillImg = styled.div``;
--- src/component/Post/PostList/styles.ts
+++ src/component/Post/PostList/styles.ts
@@ -3,4 +3,9 @@ import styled from "styled-components";
 export const Container = styled.div`
   background-color: white;
   padding-top: 30px;
+  max-height: 690px;
+  overflow-y: auto;
+  ::-webkit-scrollbar {
+    display: none;
+  }
 `;
