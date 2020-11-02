347b32036830d2cb9abdb646d5812193bd249ab7
YeongG <yeonggil223@gmail.com>
2020-11-02 23:44:39 +0900

    [MODIFY] login 테스트 콘솔

--- src/component/Login/Login.tsx
+++ src/component/Login/Login.tsx
@@ -4,6 +4,7 @@ import LoginRight from "./LoginRIght/LoginRight";
 import * as S from "../Form/styles";
 
 const Login: FC = () => {
+  console.log("랜더링");
   return (
     <S.Container>
       <FormLeft />
