236ad8c20c145880ef38545878a03fdfc49232f0
YeongG <yeonggil223@gmail.com>
2020-11-03 23:25:59 +0900

    [MODIFY] board/detail/:id 라우팅

--- src/routing/BoardRouter.tsx
+++ src/routing/BoardRouter.tsx
@@ -1,10 +1,15 @@
 import React, { FC } from "react";
 import { Route, Switch } from "react-router";
-import { BoardListContainer, BoardWriteContainer } from "../container";
+import {
+  BoardDetailContainer,
+  BoardListContainer,
+  BoardWriteContainer
+} from "../container";
 
 const BoardRouter: FC = () => {
   return (
     <Switch>
+      <Route exact path="/board/detail/:id" component={BoardDetailContainer} />
       <Route exact path="/board/list" component={BoardListContainer} />
       <Route exact path="/board/write" component={BoardWriteContainer} />
     </Switch>
