aab477cd783da0edcf835817a44b257700c5ed6f
YeongG <yeonggil223@gmail.com>
2020-11-03 23:29:23 +0900

    [CREATE] BoardWriteIntroduce component/style

---
+++ src/component/Board/BoardWrite/BoardWriteIntroduce/styles.ts
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const IntroduceInput = styled.textarea`
+  border: 1px solid #707070;
+  width: 100%;
+  resize: none;
+  font-size: 20px;
+  font-weight: bold;
+  color: #aea7a7;
+  margin-top: 40px;
+  height: 100px;
+  background-color: white;
+`;
