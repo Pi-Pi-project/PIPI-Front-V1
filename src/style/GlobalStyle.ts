2f459c017abfad72ac756ea0930b7b90461fedca
YeongG <yeonggil223@gmail.com>
2020-11-03 23:25:40 +0900

    [MODIFY] 폰트 적용

--- src/style/GlobalStyle.ts
+++ src/style/GlobalStyle.ts
@@ -6,6 +6,10 @@ export const GlobalStyle = createGlobalStyle`
         padding:0;
     }
     @import url('https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100;300;400;500;700;900&display=swap');
+
+    * {
+      font-family:Noto Sans KR;
+    }
 `;
 
 export const GlobalContainer = styled.div`
