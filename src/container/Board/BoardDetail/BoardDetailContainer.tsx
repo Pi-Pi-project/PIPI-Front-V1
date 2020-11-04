ef7c7299838fed251d5e5bd6ab78d40d93adba76
YeongG <yeonggil223@gmail.com>
2020-11-04 23:40:47 +0900

    [MODIFY] BoardDetail container

--- src/container/Board/BoardDetail/BoardDetailContainer.tsx
+++ src/container/Board/BoardDetail/BoardDetailContainer.tsx
@@ -1,7 +1,16 @@
 import React, { FC } from "react";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { RouteComponentProps } from "react-router";
 import { BoardDetail } from "../../../component";
+import { boardActionCreater } from "../../../module/action/board";
 
-const BoardDetailContainer: FC = () => {
+const BoardDetailContainer: FC<RouteComponentProps> = ({ match }) => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    const id: number = parseInt((match.params as any).id);
+    dispatch(boardActionCreater.getBoardDetailSaga(id));
+  }, []);
   return <BoardDetail />;
 };
 
