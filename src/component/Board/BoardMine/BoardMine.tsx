9b4f817d7f9a0eb3f06dae7992b55acf01a03e67
YeongG <yeonggil223@gmail.com>
2020-11-04 23:38:34 +0900

    [CREATE] BoardMine component

---
+++ src/component/Board/BoardMine/BoardMine.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from "react";
+import BoardHeader from "../BoardHeader/BoardHeader";
+
+const BoardMine: FC = () => {
+  return (
+    <div>
+      <BoardHeader activeIndex={2} />
+    </div>
+  );
+};
+
+export default BoardMine;
