b32e77d1302535b4b59d9a6aa27b93289d81e2ce
YeongG <yeonggil223@gmail.com>
2020-11-07 23:47:35 +0900

    [MODIFY] BoardRouter 수정

--- src/routing/BoardRouter.tsx
+++ src/routing/BoardRouter.tsx
@@ -1,9 +1,12 @@
 import React, { FC } from "react";
 import { Redirect, Route, Switch } from "react-router";
 import {
+  BoardApplyContainer,
   BoardDetailContainer,
   BoardListContainer,
+  BoardMemberContainer,
   BoardMineContainer,
+  BoardSearchContainer,
   BoardWriteContainer
 } from "../container";
 
@@ -14,6 +17,14 @@ const BoardRouter: FC = () => {
       <Route exact path="/board/list" component={BoardListContainer} />
       <Route exact path="/board/write" component={BoardWriteContainer} />
       <Route exact path="/board/mine" component={BoardMineContainer} />
+      <Route exact path="/board/apply" component={BoardApplyContainer} />
+      <Route
+        exact
+        path="/board/search/:category"
+        component={BoardSearchContainer}
+      />
+
+      <Route exact path="/board/member/:id" component={BoardMemberContainer} />
       <Redirect to="/board/list" />
     </Switch>
   );
