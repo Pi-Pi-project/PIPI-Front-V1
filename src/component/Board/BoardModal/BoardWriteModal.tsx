e632cab38a15f2c2aa1aeb0f5dd952a5951c34f6
YeongG <yeonggil223@gmail.com>
2020-11-07 23:57:32 +0900

    [MODIFY] BoardWriteModal component

--- src/component/Board/BoardModal/BoardWriteModal.tsx
+++ src/component/Board/BoardModal/BoardWriteModal.tsx
@@ -1,33 +1,89 @@
-import React, { FC } from "react";
-import { useSelector } from "react-redux";
+import React, { FC, ChangeEvent, useCallback } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  CategoryArr,
+  skillDataArray,
+  skillNameToSrc
+} from "../../../lib/static";
+import { writeActionCreater } from "../../../module/action/write";
 import { StoreType } from "../../../module/reducer";
+import CategoryItem from "./CategoryItem/CategoryItem";
 import * as S from "./styles";
 
 const BoardWriteModal: FC = () => {
-  const isOpen = useSelector((store: StoreType) => store.write.modalIsOpen);
+  const dispatch = useDispatch();
+  const { isOpen, type, skills, skill } = useSelector((store: StoreType) => {
+    return {
+      isOpen: store.write.modalIsOpen,
+      type: store.write.type,
+      skills: store.write.skills,
+      skill: store.write.skill
+    };
+  });
+
+  const changeHandler = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    dispatch(
+      writeActionCreater.inputUpdate({
+        name,
+        value
+      })
+    );
+  }, []);
+
+  const addSkills = useCallback((skill: string) => {
+    dispatch(writeActionCreater.setSkills(skill));
+  }, []);
+
+  const closeModal = useCallback(() => {
+    dispatch(writeActionCreater.changeModalState(false));
+  }, []);
+
   return (
     isOpen && (
-      <S.Container>
-        <S.Modal>
-          <S.CategotyItem>Web</S.CategotyItem>
-          <S.CategotyItem>App</S.CategotyItem>
-          <S.CategotyItem>
-            Data
-            <br />
-            Science
-          </S.CategotyItem>
-          <S.CategotyItem>System</S.CategotyItem>
-          <S.CategotyItem>Network</S.CategotyItem>
-          <S.CategotyItem>
-            Machine
-            <br />
-            Learning
-          </S.CategotyItem>
-          <S.CategotyItem>Security</S.CategotyItem>
-          <S.CategotyItem>Embedded</S.CategotyItem>
-          <S.CategotyItem>VR</S.CategotyItem>
-          <S.CategotyItem>Game</S.CategotyItem>
-        </S.Modal>
+      <S.Container isSecond={type === "SKILL"}>
+        <div>
+          <div>
+            <S.Modal>
+              {CategoryArr.map(category => (
+                <CategoryItem key={category} value={category} />
+              ))}
+            </S.Modal>
+          </div>
+          <S.SearchBox>
+            <S.MySkills>
+              {skills.map(src => (
+                <S.SkillImg src={skillNameToSrc[src]} />
+              ))}
+            </S.MySkills>
+            <S.SkillWrap>
+              <S.Input
+                placeholder="기술스택"
+                type="text"
+                onChange={changeHandler}
+                name="skill"
+                value={skill}
+              />
+              {skill && (
+                <S.SearchSkill>
+                  {skillDataArray
+                    .filter(data => data.searchName.includes(skill))
+                    .map(data => (
+                      <div
+                        key={data.showName}
+                        onClick={() => {
+                          addSkills(data.showName);
+                        }}
+                      >
+                        {data.showName}
+                      </div>
+                    ))}
+                </S.SearchSkill>
+              )}
+            </S.SkillWrap>
+            <S.Button onClick={closeModal}>완료</S.Button>
+          </S.SearchBox>
+        </div>
       </S.Container>
     )
   );
