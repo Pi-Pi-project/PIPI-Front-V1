50cf5c95208a882c950612dd103d0d758dc65230
YeongG <yeonggil223@gmail.com>
2020-11-07 23:53:18 +0900

    [MODIFY] PostList component

--- src/component/Post/PostList/MinePostList.tsx
+++ src/component/Post/PostList/MinePostList.tsx
@@ -1,20 +1,54 @@
 import React, { FC, UIEvent, useCallback } from "react";
 import { useDispatch } from "react-redux";
 import { boardActionCreater } from "../../../module/action/board";
+import { PostItem } from "../../../module/reducer/board";
 import { LoginedContainer } from "../../../style/GlobalStyle";
+import MinePostItem from "../PostItem/MinePostItem";
 import * as S from "./styles";
 
-const MinePostList: FC = () => {
+interface Props {
+  data: PostItem[];
+}
+
+const MinePostList: FC<Props> = ({ data }) => {
   const dispatch = useDispatch();
   const onScrollHandler = useCallback((e: UIEvent<HTMLDivElement>) => {
     const target = e.target as HTMLLIElement;
     if (target.scrollHeight - target.clientHeight === target.scrollTop) {
-      dispatch(boardActionCreater.getBoardListSaga());
     }
   }, []);
   return (
     <S.Container onScroll={onScrollHandler}>
-      <LoginedContainer></LoginedContainer>
+      <LoginedContainer>
+        {data.map(
+          ({
+            category,
+            userNickname,
+            id,
+            title,
+            createdAt,
+            idea,
+            img,
+            postSkillsets,
+            userEmail,
+            userImg
+          }) => (
+            <MinePostItem
+              key={id}
+              category={category}
+              userNickname={userNickname}
+              id={id}
+              title={title}
+              createdAt={createdAt}
+              idea={idea}
+              img={img}
+              postSkillsets={postSkillsets}
+              userEmail={userEmail}
+              userImg={userImg}
+            />
+          )
+        )}
+      </LoginedContainer>
     </S.Container>
   );
 };
