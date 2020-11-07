5d04277753dace81381ee29452d62a2960da879e
YeongG <yeonggil223@gmail.com>
2020-11-07 23:52:28 +0900

    [MODIFY] BoardList container

--- src/container/Board/BoardList/BoardListContainer.tsx
+++ src/container/Board/BoardList/BoardListContainer.tsx
@@ -7,6 +7,7 @@ import { boardActionCreater } from "../../../module/action/board";
 const BoardListContainer: FC = () => {
   const dispatch = useDispatch();
   useEffect(() => {
+    dispatch(boardActionCreater.resetBoard());
     dispatch(boardActionCreater.getBoardListSaga());
   }, []);
   return <BoardList />;
