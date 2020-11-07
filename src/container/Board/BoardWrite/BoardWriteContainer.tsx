fbc59c9b3ee8cf84ae6c07166f56cae99b359e95
YeongG <yeonggil223@gmail.com>
2020-11-07 23:52:54 +0900

    [MODIFY] BoardWrite container

--- src/container/Board/BoardWrite/BoardWriteContainer.tsx
+++ src/container/Board/BoardWrite/BoardWriteContainer.tsx
@@ -1,7 +1,16 @@
 import React, { FC } from "react";
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { useHistory } from "react-router";
 import { BoardWrtie } from "../../../component";
+import { StoreType } from "../../../module/reducer";
 
 const BoardWriteContainer: FC = () => {
+  const history = useHistory();
+  const success = useSelector((store: StoreType) => store.write.success);
+  useEffect(() => {
+    success && history.push("/board/list");
+  }, [success]);
   return <BoardWrtie />;
 };
 
