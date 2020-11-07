28d7084915a4e426b956a01754ca6a74e9461392
YeongG <yeonggil223@gmail.com>
2020-11-07 23:54:01 +0900

    [CREATE] PostList component

---
+++ src/component/Post/PostList/SearchPostList.tsx
@@ -0,0 +1,66 @@
+import React, { FC, UIEvent } from "react";
+import { useCallback } from "react";
+import { useDispatch } from "react-redux";
+import { useHistory } from "react-router";
+import { boardActionCreater } from "../../../module/action/board";
+import { PostItem } from "../../../module/reducer/board";
+import { LoginedContainer } from "../../../style/GlobalStyle";
+import ViewPostItem from "../PostItem/ViewPostItem";
+import * as S from "./styles";
+
+interface Props {
+  data: PostItem[];
+}
+
+const SearchPostList: FC<Props> = ({ data }) => {
+  const dispatch = useDispatch();
+  const history = useHistory();
+
+  const onScrollHandler = useCallback((e: UIEvent<HTMLDivElement>) => {
+    const target = e.target as HTMLLIElement;
+    if (target.scrollHeight - target.clientHeight === target.scrollTop) {
+      dispatch(
+        boardActionCreater.getBoardSearchSaga(
+          history.location.pathname.split("/")[3]
+        )
+      );
+    }
+  }, []);
+
+  return (
+    <S.Container onScroll={onScrollHandler}>
+      <LoginedContainer>
+        {data.map(
+          ({
+            category,
+            userNickname,
+            id,
+            title,
+            createdAt,
+            idea,
+            img,
+            postSkillsets,
+            userEmail,
+            userImg
+          }) => (
+            <ViewPostItem
+              key={id}
+              category={category}
+              userNickname={userNickname}
+              id={id}
+              title={title}
+              createdAt={createdAt}
+              idea={idea}
+              img={img}
+              postSkillsets={postSkillsets}
+              userEmail={userEmail}
+              userImg={userImg}
+            />
+          )
+        )}
+      </LoginedContainer>
+    </S.Container>
+  );
+};
+
+export default SearchPostList;
