c3916f612a7c26b0631dda4003bd1fbfccb5ec54
YeongG <yeonggil223@gmail.com>
2020-11-07 23:54:51 +0900

    [MODIFY] BoardMine component

--- src/component/Board/BoardMine/BoardMine.tsx
+++ src/component/Board/BoardMine/BoardMine.tsx
@@ -1,10 +1,15 @@
 import React, { FC } from "react";
+import { useSelector } from "react-redux";
+import { StoreType } from "../../../module/reducer";
+import MinePostList from "../../Post/PostList/MinePostList";
 import BoardHeader from "../BoardHeader/BoardHeader";
 
 const BoardMine: FC = () => {
+  const list = useSelector((store: StoreType) => store.board.boardDoing.list);
   return (
     <div>
       <BoardHeader activeIndex={2} />
+      <MinePostList data={list} />
     </div>
   );
 };
