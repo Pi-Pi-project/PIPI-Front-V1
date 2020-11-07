557dec94633ca83b57a49226bef854341bab7b02
YeongG <yeonggil223@gmail.com>
2020-11-07 23:50:48 +0900

    [MODIFY] rootSaga

--- src/module/saga/index.ts
+++ src/module/saga/index.ts
@@ -1,11 +1,20 @@
 import { all } from "redux-saga/effects";
 import boardSaga from "./board";
 import loginSaga from "./login";
+import navigationSaga from "./navigation";
 import passwordSaga from "./password";
 import registerSaga from "./register";
+import writeSaga from "./write";
 
 function* rootSaga() {
-  yield all([loginSaga(), passwordSaga(), registerSaga(), boardSaga()]);
+  yield all([
+    loginSaga(),
+    passwordSaga(),
+    registerSaga(),
+    boardSaga(),
+    writeSaga(),
+    navigationSaga()
+  ]);
 }
 
 export default rootSaga;
