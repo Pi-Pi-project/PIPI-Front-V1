735d69241e9ecdabdc754650e179e5acde10ee46
YeongG <yeonggil223@gmail.com>
2020-11-07 23:57:52 +0900

    [MODIFY] BoardDetailHeader component

--- src/component/Board/BoardDetail/BoardDetailHeader/BoardDetailHeader.tsx
+++ src/component/Board/BoardDetail/BoardDetailHeader/BoardDetailHeader.tsx
@@ -1,6 +1,6 @@
 import React, { FC } from "react";
 import { useSelector } from "react-redux";
-import { BASE_URL } from "../../../../lib/api";
+import { BASE_IMG_URL } from "../../../../lib/api";
 import { StoreType } from "../../../../module/reducer";
 import * as S from "../styles";
 
@@ -12,7 +12,7 @@ const BoardDetailHeader: FC = () => {
     <S.BoardDetailHeader>
       <S.ProjectName>{title}</S.ProjectName>
       <S.ProfileWrap>
-        <S.ProfileImg src={`${BASE_URL}/image/${userImg}`} />
+        <S.ProfileImg src={`${BASE_IMG_URL}/${userImg}`} />
         <S.ProfileName>{userNickname}</S.ProfileName>
       </S.ProfileWrap>
     </S.BoardDetailHeader>
