1e2f252e904cd233e940dabdff204cfba803393b
YeongG <yeonggil223@gmail.com>
2020-11-11 23:10:43 +0900

    [MODIFY] 사진 클릭하면 프로필로 이동

--- src/component/Navigation/Navigation.tsx
+++ src/component/Navigation/Navigation.tsx
@@ -1,8 +1,9 @@
-import React, { FC } from "react";
+import React, { FC, useCallback } from "react";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { useHistory } from "react-router";
 import { pipiLogoSvg } from "../../asset";
-import { BASE_IMG_URL } from "../../lib/api";
+import { getImgSrc } from "../../lib/func";
 import { navigationActionCreater } from "../../module/action/navigation";
 import { StoreType } from "../../module/reducer";
 import { LoginedContainer } from "../../style/GlobalStyle";
@@ -10,20 +11,29 @@ import * as S from "./styles";
 
 const Navigation: FC = () => {
   const dispatch = useDispatch();
+  const history = useHistory();
   const { email, profileImg } = useSelector(
     (store: StoreType) => store.navigation
   );
   useEffect(() => {
     dispatch(navigationActionCreater.getUserInfoSaga());
   }, []);
+
+  const gotoProfile = useCallback(() => {
+    history.push(`/board/profile?email=${email}`);
+  }, [email]);
+
+  const gotoMain = useCallback(() => {
+    history.push("/board/list");
+  }, []);
   return (
     <S.Container>
       <LoginedContainer>
-        <div>
+        <div onClick={gotoMain}>
           <S.ImgCircle src={pipiLogoSvg} />
           <S.LogoText>PiPi</S.LogoText>
         </div>
-        <S.ImgCircle src={`${BASE_IMG_URL}/${profileImg}`} />
+        <S.ImgCircle onClick={gotoProfile} src={getImgSrc(profileImg)} />
       </LoginedContainer>
     </S.Container>
   );
