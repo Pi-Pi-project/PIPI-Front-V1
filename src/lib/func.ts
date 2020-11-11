e79e77a3cd8dd6cd0fdfa3b14393b38ede1f9b83
YeongG <yeonggil223@gmail.com>
2020-11-11 23:02:21 +0900

    [MODIFY] json => function

--- src/lib/func.ts
+++ src/lib/func.ts
@@ -1,5 +1,12 @@
+import { BASE_IMG_URL } from "./api";
+import { skillNameToSrc } from "./static";
+
 export const checkEnterKeyCode = (keyCode: string): boolean => {
   const ENTER = "Enter";
   if (keyCode === ENTER) return true;
   return false;
 };
+
+export const getImgSrc = (src: string) => `${BASE_IMG_URL}/${src}`;
+
+export const getSkillImgSrc = (skillName: string) => skillNameToSrc[skillName];
