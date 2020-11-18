a913a213e729ab44e38021c2f3083c4f232d6828
YeongG <yeonggil223@gmail.com>
2020-11-18 22:16:10 +0900

    [MODIFY] 컴포넌트 이름 변경

--- src/container/Profile/ProfileContainer.tsx
+++ src/container/Profile/ProfileContainer.tsx
@@ -1,17 +1,18 @@
 import React, { FC, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { RouteChildrenProps } from "react-router";
-import { BoardProfile } from "../../../component";
-import { profileActionCreater } from "../../../module/action/profile";
+import { BoardProfile } from "../../component";
+import { profileActionCreater } from "../../module/action/profile";
 import querySring from "query-string";
 
-const BoardProfileContainer: FC<RouteChildrenProps> = ({ location }) => {
+const ProfileContainer: FC<RouteChildrenProps> = ({ location }) => {
   const dispatch = useDispatch();
   useEffect(() => {
     const { email } = querySring.parse(location.search);
     dispatch(profileActionCreater.getProfileSaga(email as string));
+    dispatch(profileActionCreater.getPortfolioSaga(email as string));
   }, []);
   return <BoardProfile />;
 };
 
-export default BoardProfileContainer;
+export default ProfileContainer;
