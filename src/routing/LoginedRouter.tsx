4db674f87cf578565bf59b6d280584c3b6e18231
YeongG <yeonggil223@gmail.com>
2020-11-18 22:36:54 +0900

    [MODIFY] 로그인 되있지 않을시 로그인 으로

--- src/routing/LoginedRouter.tsx
+++ src/routing/LoginedRouter.tsx
@@ -1,14 +1,24 @@
-import React, { FC } from "react";
-import { Route, Switch } from "react-router";
+import React, { FC, useEffect } from "react";
+import { Route, Switch, useHistory } from "react-router";
 import Navigation from "../component/Navigation/Navigation";
+import { ProfileContainer, ManagementContainer } from "../container";
+import { checkIsLogin } from "../lib/api";
 import * as G from "../style/GlobalStyle";
 import BoardRouter from "./BoardRouter";
 
 const LoginedRouter: FC = () => {
+  const history = useHistory();
+  useEffect(() => {
+    checkIsLogin().then(res => {
+      !res && history.push("/login");
+    });
+  }, []);
   return (
     <>
       <Navigation />
       <Switch>
+        <Route exact path="/profile" component={ProfileContainer} />
+        <Route exact path="/management/:id" component={ManagementContainer} />
         <Route component={BoardRouter} />
       </Switch>
     </>
