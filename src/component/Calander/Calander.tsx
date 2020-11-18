4675206d541cae9faafc6e370718833a39d38a9d
YeongG <yeonggil223@gmail.com>
2020-11-18 22:39:13 +0900

    [CREATE] Calander component/style

---
+++ src/component/Calander/Calander.tsx
@@ -0,0 +1,70 @@
+import React, { FC, useCallback, useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import WindowCalander, {
+  WindowCalander as WindowCalanderType
+} from "window-calander";
+import { getMonthName } from "../../lib/func";
+import { managementActionCreater } from "../../module/action/management";
+import { StoreType } from "../../module/reducer";
+import ToDoList from "../ToDoList/ToDoList";
+import CalanderLine from "./CalanderLine/CalanderLine";
+import * as S from "./styles";
+
+const Calander: FC = () => {
+  const dispatch = useDispatch();
+  const [list, setList] = useState<WindowCalanderType>([]);
+
+  const { month, year, selectDate, todos } = useSelector(
+    (store: StoreType) => store.management
+  );
+
+  useEffect(() => {
+    setList(WindowCalander(year, month));
+  }, [month, year]);
+
+  useEffect(() => {
+    const { year, date, month } = selectDate;
+    if (year === 0) return;
+    dispatch(managementActionCreater.getTodoSaga());
+  }, [selectDate]);
+
+  const nextMonth = useCallback(() => {
+    dispatch(managementActionCreater.nextMonth());
+  }, []);
+  const prevMonth = useCallback(() => {
+    dispatch(managementActionCreater.prevMonth());
+  }, []);
+
+  return (
+    <S.Container>
+      <S.CalanderLeft>
+        <div>
+          <S.CalanderLeftHeader>
+            <S.ButtonWrap onClick={prevMonth}>&lt;</S.ButtonWrap>
+            <S.BigText>
+              {getMonthName(month)} {year}
+            </S.BigText>
+            <S.ButtonWrap onClick={nextMonth}>&gt;</S.ButtonWrap>
+          </S.CalanderLeftHeader>
+          <S.CalanderLineDiv>
+            <S.CalanderItemDiv>S</S.CalanderItemDiv>
+            <S.CalanderItemDiv>M</S.CalanderItemDiv>
+            <S.CalanderItemDiv>T</S.CalanderItemDiv>
+            <S.CalanderItemDiv>W</S.CalanderItemDiv>
+            <S.CalanderItemDiv>T</S.CalanderItemDiv>
+            <S.CalanderItemDiv>F</S.CalanderItemDiv>
+            <S.CalanderItemDiv>S</S.CalanderItemDiv>
+          </S.CalanderLineDiv>
+        </div>
+        {list.map((data, i) => (
+          <CalanderLine key={i} selectDate={selectDate} data={data} />
+        ))}
+      </S.CalanderLeft>
+      <S.TodoRight>
+        <ToDoList todos={todos} />
+      </S.TodoRight>
+    </S.Container>
+  );
+};
+
+export default Calander;
