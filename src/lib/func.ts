ca843afc2e54bbfacfae02471efc45e89f865e66
YeongG <yeonggil223@gmail.com>
2020-11-18 22:17:19 +0900

    [MODIFY] func.ts getMonthName 추가

--- src/lib/func.ts
+++ src/lib/func.ts
@@ -1,5 +1,5 @@
 import { BASE_IMG_URL } from "./api";
-import { skillNameToSrc } from "./static";
+import { monthName, skillNameToSrc } from "./static";
 
 export const checkEnterKeyCode = (keyCode: string): boolean => {
   const ENTER = "Enter";
@@ -10,3 +10,8 @@ export const checkEnterKeyCode = (keyCode: string): boolean => {
 export const getImgSrc = (src: string) => `${BASE_IMG_URL}/${src}`;
 
 export const getSkillImgSrc = (skillName: string) => skillNameToSrc[skillName];
+
+export const getMonthName = (monthIndex: number) => monthName[monthIndex - 1];
+
+export const padStart = (value: string | number, length: number): string =>
+  `${value}`.padStart(length, "0");
