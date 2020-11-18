e4f587f5f0144c1beaaa3e7219c4041bb3ffe835
YeongG <yeonggil223@gmail.com>
2020-11-18 22:41:33 +0900

    [MODIFY] 로그인 되있으면 자동 로그인

--- src/container/Login/LoginContainer.tsx
+++ src/container/Login/LoginContainer.tsx
@@ -3,6 +3,7 @@ import { useEffect } from "react";
 import { useSelector } from "react-redux";
 import { useHistory } from "react-router";
 import { Login } from "../../component";
+import { checkIsLogin } from "../../lib/api";
 import { StoreType } from "../../module/reducer";
 
 const LoginContainer: FC = () => {
@@ -10,6 +11,11 @@ const LoginContainer: FC = () => {
   const successLogin = useSelector(
     (store: StoreType) => store.login.successLogin
   );
+  useEffect(() => {
+    checkIsLogin().then(res => {
+      res && history.push("/board/list");
+    });
+  }, []);
 
   useEffect(() => {
     successLogin && history.push("/board/list");
