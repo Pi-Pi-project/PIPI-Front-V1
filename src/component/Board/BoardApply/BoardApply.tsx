c6a00dd693dd79990846f7d5bb48566bca6d04f8
YeongG <yeonggil223@gmail.com>
2020-11-26 23:40:46 +0900

    [MODIFY] 리스트 없을시 화면 추가

--- src/component/Board/BoardApply/BoardApply.tsx
+++ src/component/Board/BoardApply/BoardApply.tsx
@@ -2,14 +2,25 @@ import React, { FC } from "react";
 import { useSelector } from "react-redux";
 import { StoreType } from "../../../module/reducer";
 import ApplyPostList from "../../Post/PostList/ApplyPostList";
+import NoneList from "../../Post/PostList/NoneList";
 import BoardHeader from "../BoardHeader/BoardHeader";
 
 const BoardApply: FC = () => {
-  const list = useSelector((store: StoreType) => store.board.boardApply.list);
+  const { list, isLoading } = useSelector(
+    (store: StoreType) => store.board.boardApply
+  );
   return (
     <div>
       <BoardHeader activeIndex={3} />
-      <ApplyPostList data={list} />
+      {isLoading ? (
+        list.length ? (
+          <ApplyPostList data={list} />
+        ) : (
+          <NoneList />
+        )
+      ) : (
+        ""
+      )}
     </div>
   );
 };
