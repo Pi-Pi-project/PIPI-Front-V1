fb75b2d7722541cb6b2d4fdcc3521c634a5d45a7
YeongG <yeonggil223@gmail.com>
2020-11-26 23:38:16 +0900

    [MODIFY] component index.ts

--- src/asset/index.ts
+++ src/asset/index.ts
@@ -4,3 +4,4 @@ export { default as magnifyingIconSvg } from "./board/magnifying-icon.svg";
 export { default as pictureIcon } from "./form/picture.svg";
 export { default as todoSuccessSvg } from "./todolist/todoSuccess.svg";
 export { default as todoFailSvg } from "./todolist/todoFail.svg";
+export { default as monsterSvg } from "./board/monster.svg";
