f299d295a3c256ce4022703ab24f082f37be285c
YeongG <yeonggil223@gmail.com>
2020-11-26 23:39:39 +0900

    [MODIFY] AdminItem component

--- src/component/Admin/AdminItem/AdminItem.tsx
+++ src/component/Admin/AdminItem/AdminItem.tsx
@@ -1,12 +1,28 @@
 import React, { FC } from "react";
+import { getImgSrc } from "../../../lib/func";
+import {
+  ProjectApplyItem,
+  ReportItem
+} from "../../../module/reducer/AdminMain";
 import * as S from "./styles";
 
-const AdminItem: FC = () => {
+interface Props {
+  profileImg: string;
+  reportedEmail: string;
+  reporterEmail: string;
+  userNickname: string;
+}
+const AdminItem: FC<Props> = ({
+  profileImg,
+  reportedEmail,
+  reporterEmail,
+  userNickname
+}) => {
   return (
     <S.Container>
       <div>
-        <S.Img src="https://lh3.googleusercontent.com/proxy/qzXgRq0cSpQRIbEyepp46OwAF9YscV1eakFcNKolFZ_rOJH9TDRSMXhdxsbvDexMm4hJ2A8B0_6FAxhCNIPW8pHvQzvxzeRLM4Q4fopcJTjx1eYaW_r6T12yRBtNXQkGzymtYefpxoQ5W1tySeQ7SG5_6T-38ejWyaL29sD7ode0uhClLkYcWmnZbLRyXA" />
-        <S.Span>불건전한닉네임</S.Span>
+        <S.Img src={getImgSrc(profileImg || "")} />
+        <S.Span>{userNickname}</S.Span>
       </div>
       <div>
         <S.Button color="#3B3B3B">확인</S.Button>
