6f5a94aff812fe1235d3fa1a77896fc115f62d0b
YeongG <yeonggil223@gmail.com>
2020-12-02 10:10:33 +0900

    [MODIFY] redux adminMain action/saga

--- src/module/saga/adminMain/index.ts
+++ src/module/saga/adminMain/index.ts
@@ -4,6 +4,7 @@ import {
   adminMainAction,
   adminMainActionCreater
 } from "../../action/adminMain";
+import { modalActionCreater } from "../../action/modal";
 
 function* getProjectSaga() {
   try {
@@ -25,10 +26,45 @@ function* getReportSaga() {
     yield put(adminMainActionCreater.getReport(res.data));
   } catch (err) {}
 }
+function* getReportDetailSaga(
+  action: ReturnType<typeof adminMainActionCreater.getReportDetailSaga>
+) {
+  try {
+    const { reportedEmail, reporterEmail } = action.payload;
+    const res = yield call(
+      requestApiWithAccessToken,
+      "get",
+      `/admin/report/detail?reportedEmail=${reportedEmail}&reporterEmail=${reporterEmail}`
+    );
+    yield put(
+      modalActionCreater.applyDetailModalOn({ type: "REPORT", ...res.data })
+    );
+  } catch (err) {}
+}
+function* getProjectDetailSaga(
+  action: ReturnType<typeof adminMainActionCreater.getProjectDetailSaga>
+) {
+  try {
+    const res = yield call(
+      requestApiWithAccessToken,
+      "get",
+      `/admin/project/detail?id=${action.payload}`
+    );
+
+    yield put(
+      modalActionCreater.applyDetailModalOn({ type: "PROJECT", ...res.data })
+    );
+  } catch (err) {}
+}
 
 function* adminMainSaga() {
   yield takeEvery(adminMainAction.GET_PROJECT_SAGA, getProjectSaga);
   yield takeEvery(adminMainAction.GET_REPORT_SAGA, getReportSaga);
+  yield takeEvery(adminMainAction.GET_REPORT_DETAIL_SAGA, getReportDetailSaga);
+  yield takeEvery(
+    adminMainAction.GET_PROJECT_DETAIL_SAGA,
+    getProjectDetailSaga
+  );
 }
 
 export default adminMainSaga;
