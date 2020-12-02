18d7126b7cd979c03f6b6f6b93681d85e5545588
YeongG <yeonggil223@gmail.com>
2020-12-02 10:02:06 +0900

    [MODIFY] 폰트 이슈 해결

--- src/style/GlobalStyle.ts
+++ src/style/GlobalStyle.ts
@@ -5,10 +5,8 @@ export const GlobalStyle = createGlobalStyle`
         margin:0;
         padding:0;
     }
-    @import url('https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100;300;400;500;700;900&display=swap');
-
     * {
-      font-family:Noto Sans KR;
+      font-family:Noto Sans KR !important;
     }
 `;
 
