c7c9bff4c7454175a3c7a1239b5aee15b0f7ff1a
YeongG <yeonggil223@gmail.com>
2020-12-02 10:06:53 +0900

    [CREATE] ChatItem component/style

---
+++ src/component/ChatItem/ChatItem.tsx
@@ -0,0 +1,40 @@
+import React, { FC } from "react";
+import { getImgSrc } from "../../lib/func";
+import { ChatItme } from "../Chat/Chat";
+import * as S from "./styles";
+
+interface Props extends ChatItme {
+  isMine: boolean;
+}
+
+const ChatItem: FC<Props> = ({ isMine, email, content, img, nickname }) => {
+  return (
+    <S.Container isMine={isMine}>
+      {isMine ? (
+        <>
+          <S.UserWrap>
+            <S.Content>{content}</S.Content>
+          </S.UserWrap>
+          {nickname && (
+            <S.ProfileWrap>
+              <S.UserName isMine={isMine}>{nickname}</S.UserName>
+              <S.ProfileImg src={getImgSrc(img)} />
+            </S.ProfileWrap>
+          )}
+        </>
+      ) : (
+        <>
+          <S.ProfileWrap>
+            <S.UserName isMine={isMine}>{nickname}</S.UserName>
+            <S.ProfileImg src={getImgSrc(img)} />
+          </S.ProfileWrap>
+          <S.UserWrap>
+            <S.Content>{content}</S.Content>
+          </S.UserWrap>
+        </>
+      )}
+    </S.Container>
+  );
+};
+
+export default ChatItem;
