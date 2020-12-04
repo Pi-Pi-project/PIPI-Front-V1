c0a5b8abce314de6074834cdd353c0fe3edea99d
YeongG <yeonggil223@gmail.com>
2020-12-04 23:38:22 +0900

    [MODIFY] ViewPost랜더링시 인공지능 가져오기

--- src/component/Post/PostList/ViewPostList.tsx
+++ src/component/Post/PostList/ViewPostList.tsx
@@ -1,11 +1,14 @@
-import React, { FC, UIEvent } from "react";
+import Axios from "axios";
+import React, { FC, UIEvent, useEffect, useState } from "react";
 import { useCallback } from "react";
 import { useRef } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { useHistory } from "react-router";
 import { boardActionCreater } from "../../../module/action/board";
+import { StoreType } from "../../../module/reducer";
 import { PostItem } from "../../../module/reducer/board";
 import { LoginedContainer } from "../../../style/GlobalStyle";
+import CategoryItem from "../../Board/BoardModal/CategoryItem/CategoryItem";
 import ViewPostItem from "../PostItem/ViewPostItem";
 import * as S from "./styles";
 
@@ -15,6 +18,8 @@ interface Props {
 
 const ViewPostList: FC<Props> = ({ data }) => {
   const dispatch = useDispatch();
+  const [aiRecommend, setAiRecommend] = useState<PostItem>(null);
+  const email = useSelector((store: StoreType) => store.navigation.email);
   const onScrollHandler = useCallback((e: UIEvent<HTMLDivElement>) => {
     const target = e.target as HTMLLIElement;
     if (target.scrollHeight - target.clientHeight === target.scrollTop) {
@@ -22,9 +27,25 @@ const ViewPostList: FC<Props> = ({ data }) => {
     }
   }, []);
 
+  useEffect(() => {
+    if (!email) return;
+    Axios.post("http://172.20.10.2:5000/output", {
+      email
+    }).then(res => {
+      setAiRecommend(res.data);
+    });
+  }, [email]);
+
   return (
     <S.Container onScroll={onScrollHandler}>
       <LoginedContainer>
+        {aiRecommend && (
+          <S.AiRecommend>
+            <span>인공지능의 추천!</span>
+            {<ViewPostItem {...aiRecommend} />}
+          </S.AiRecommend>
+        )}
+
         {data.map(
           ({
             category,
