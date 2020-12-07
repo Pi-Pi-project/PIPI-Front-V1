a343d11ec8a19aef388740fae697d32ba585f20c
YeongG <yeonggil223@gmail.com>
2020-12-07 15:46:41 +0900

    [MODIFY] adminMain 수락시 모달 온

--- src/component/Admin/AdminItem/ProjectAdminItem.tsx
+++ src/component/Admin/AdminItem/ProjectAdminItem.tsx
@@ -1,6 +1,7 @@
 import React, { FC, useCallback } from "react";
 import { useDispatch } from "react-redux";
 import { adminMainActionCreater } from "../../../module/action/adminMain";
+import { modalActionCreater } from "../../../module/action/modal";
 import { ProjectApplyItem } from "../../../module/reducer/AdminMain";
 import * as S from "./styles";
 
@@ -9,6 +10,15 @@ const ProjectAdminItem: FC<ProjectApplyItem> = ({ projectId, title }) => {
   const onProjectModal = useCallback(() => {
     dispatch(adminMainActionCreater.getProjectDetailSaga(projectId));
   }, []);
+  const success = useCallback(() => {
+    dispatch(
+      modalActionCreater.formModalOn({
+        title: "성공했습니다",
+        subTitle: "프로젝트 완료 신청을 수락했습니다"
+      })
+    );
+  }, []);
+
   return (
     <S.Container>
       <div>
@@ -19,7 +29,9 @@ const ProjectAdminItem: FC<ProjectApplyItem> = ({ projectId, title }) => {
           확인
         </S.Button>
         <S.Button color="#3B3B3B">거절</S.Button>
-        <S.Button color="#FC2F2F">승낙</S.Button>
+        <S.Button onClick={success} color="#FC2F2F">
+          승낙
+        </S.Button>
       </div>
     </S.Container>
   );
